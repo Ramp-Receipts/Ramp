a314fcb8eacd78f902794756b731ff561a3357bc
Miroslav Popovic <miroslav.popovic@gmail.com>
2017-05-17 22:33:10 +0200

    Implement a monthly receipt view functionality

--- client/src/Receipt.js
+++ client/src/Receipt.js
@@ -5,28 +5,24 @@ import numeral from 'numeral';
 
 class Receipt extends Component {
 
-  // state = {
-  //   loaded: false,
-  //   receipts: []
-  // };
+  state = {
+    loaded: false,
+    receipt: {charges:[]}
+  };
 
   componentDidMount() {
-    console.log('Did mount');
-    // fetch('/receipts')
-    //   .then(res => res.json())
-    //   .then(receipts => this.setState({
-    //     loaded: true,
-    //     receipts
-    //   }));
-  }
+    let params = this.props.match.params;
+    let year = parseInt(params.year);
+    let month = parseInt(params.month);
 
-  componentWillReceiveProps(properties) {
-    console.log(properties);
-    // this.setState({
-    //   stripeAccounts: properties.stripeAccounts,
-    //   user: properties.user,
-    //   stripeConfig: properties.stripeConfig
-    // });
+    this.setState({ year, month });
+
+    fetch(`/receipts/${year}/${month}`)
+      .then(res => res.json())
+      .then(receipt => this.setState({
+        loaded: true,
+        receipt
+      }));
   }
 
   formatCurrency(value) {
@@ -43,10 +39,41 @@ class Receipt extends Component {
         Loading data...
       </div>
 
+    let receipt =
+      <div>
+        <table className="table table-hover table-striped">
+          <thead>
+            <tr>
+              <th>Description</th>
+              <th className="text-right">Amount</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.receipt.charges.map((charge, index) =>
+            <tr key={index}>
+              <td>
+                {charge.description || '-'}
+              </td>
+              <td className="text-right">
+                  {this.formatCurrency(charge.totalAmount)}
+              </td>
+            </tr>)}
+          </tbody>
+        </table>
+
+        <hr />
+
+        <div className="text-right">
+          <strong>
+            Total: {this.formatCurrency(this.state.receipt.totalAmount)}
+          </strong>
+        </div>
+      </div>
+
     return (
       <div>
-        <h1>Receipt for </h1>
-        { loading }
+        <h1>Receipt for {this.formatMonth(this.state.year, this.state.month)}</h1>
+        {!this.state.loaded ? loading : receipt}
       </div>
     );
   }
--- routes/receipts.js
+++ routes/receipts.js
@@ -24,4 +24,25 @@ router.get('/', (req, res, next) => {
   });
 });
 
+router.get('/:year/:month', (req, res, next) => {
+  let year = req.params.year;
+  let month = req.params.month;
+
+  request.get({
+    url: `${rootUrl}${customerId}/${year}/${month}`,
+    headers: {
+      'Authorization': `Token ${accessKey}`
+    },
+    json: true
+  }, (error, response, body) => {
+    if (error) {
+      let errorMessage = `Error calling Ramp Receipts API: ${error}`;
+      console.log(errorMessage);
+      res.status(500).json({error: errorMessage});
+    } else {
+      res.json(body);
+    }
+  });
+});
+
 module.exports = router;
